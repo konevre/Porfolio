c4627def3afb41e70641d08da4f7d8d2cfdc8ab5
konevre <konevre@mail.ru>
2023-04-04 12:40:25 +0700

    changed script

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,10 +1,11 @@
 const { merge } = require("webpack-merge");
-const baseConfig = require("./webpack.config.js");
+const baseConfig = require("./webpack.config");
 
 module.exports = merge(baseConfig, {
     mode: "production",
     devtool: false,
     output: {
+        publicPath: "./",
         filename: "bundle.js",
         path: path.resolve(__dirname, "dist"),
     },
